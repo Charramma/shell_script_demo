b2b7b30327a8f577d5c09695d0e11271949b9880
huang.zyn@qq.com <huang.zyn@qq.com>
2020-12-14 14:27:22 +0800

    2020-12-14

--- README.md
+++ README.md
@@ -43,3 +43,6 @@
 - **ip_change.sh** 配置静态ip
 
 - **nfsserver_build.sh** nfs服务端配置脚本，根据需求对配置进行修改
+
+- **python3_9_1_setup_C7.sh** CentOS 7 环境下编译安装Python3.9.1，安装路径在/usr/local/python39/
+
---
+++ python3_9_1_setup_C7.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Date: 2020-12-14
+# Author: Charramma(Huang)
+# Description: CentOS 7 编译安装Python 3.9.1
+
+# 安装编译所需要的工具包
+yum install epel-release -y
+yum install wget make -y
+# 安装依赖
+yum install gcc patch libffi-devel python-devel zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel -y
+
+# 下载python3.9.1的源码包
+wget -N https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz
+#解压
+tar -xzf Python-3.9.1.tgz
+cd Python-3.9.1
+
+# 编译安装三部曲
+./configure --prefix=/usr/local/python39/
+make -j2
+make insatll
+
+# 添加安装目录到PATH变量
+export PATH=$PATH:/usr/local/python39/bin/
+echo "export PATH=$PATH:/usr/local/python39/bin/" >> /etc/profile
+
+# 配置pip国内源
+mkdir ~/.pip
+cat > ~/.pip/pip.conf << EOF
+[global]
+time-out=60
+index-url = https://pypi.tuna.tsinghua.edu.cn/simple/
+
+[install]
+trusted-host=tsinghua.edu.cn
+EOF
+
+# 检验安装
+python3 --version
+if [[ $? == 0 ]]; then
+    echo -e "\033[42;37m Python 3.9.1 安装成功！ \033[0m"
+else
+    echo -e "\033[41;37m Python 3.9.1 安装失败！ \033[0m"
+fi
